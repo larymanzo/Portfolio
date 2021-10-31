362596fb0c2f4a57f4a03a17cacc6c08a15f8761
larymanzo <lamosinha@hotmail.com>
2021-10-30 23:34:57 -0300

    Alterações em contato

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -8,11 +8,11 @@ class Contato extends React.Component {
     return (
       <>
         <Header />
-        <section className="welcome nav-contact">
-          <a className="" href="//www.github.com/larymanzo" alt="github">
+        <section className="welcome">
+          <a className="nav-contact" href="//www.github.com/larymanzo" alt="github">
             Github
           </a>
-          <a className="" href="//www.linkedin.com/in/larissa-manzo/" alt="github">
+          <a className="nav-contact" href="//www.linkedin.com/in/larissa-manzo/" alt="github">
             Linkedin
           </a>
         </section>
