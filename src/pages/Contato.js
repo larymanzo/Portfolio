084f90c8c5adc38442d141a563c7fd9a0529a034
larymanzo <lamosinha@hotmail.com>
2021-10-31 23:28:32 -0300

    Cards projetos e icones contatos

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import contato from '../images/contato.jpg'
+import github from '../images/github.png'
+import linkedin from '../images/linkedin.png'
+import whatsapp from '../images/whatsapp.png'
 import '../css/main.css'
 
 class Contato extends React.Component {
@@ -16,9 +19,30 @@ class Contato extends React.Component {
           <div className="contact-right">
             <h2>Contato</h2>
             <div className="entalhe" />
-            <a href="https://github.com/larymanzo">github.com/larymanzo</a>
-            <a href="https://www.linkedin.com/in/larissa-manzo/">linkedin.com/in/larissa-manzo</a>
-            <a href="https://api.whatsapp.com/send?phone=+5514998464393&text=Ol%C3%A1,%20vim%20atrav%C3%A9s%20do%20seu%20portif%C3%B3lio%20Web!">whatsapp</a>
+            <a
+              className="link-contact"
+              href="https://github.com/larymanzo"
+              target="_blank"
+              rel="noreferrer">
+                <img className="icon" src={github} alt="icone github" />
+                github.com/larymanzo
+            </a>
+            <a
+              className="link-contact"
+              href="https://www.linkedin.com/in/larissa-manzo/"
+              target="_blank"
+              rel="noreferrer">
+                <img className="icon" src={linkedin} alt="icone github" />
+                linkedin.com/in/larissa-manzo
+            </a>
+            <a
+              className="link-contact"
+              href="https://api.whatsapp.com/send?phone=+5514998464393&text=Ol%C3%A1,%20vim%20atrav%C3%A9s%20do%20seu%20portif%C3%B3lio%20Web!"
+              target="_blank"
+              rel="noreferrer">
+                <img className="icon" src={whatsapp} alt="icone github" />
+                whatsapp
+            </a>
           </div>
         </section>
         <Footer />
--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -9,61 +9,62 @@ class Projetos extends React.Component {
       <>
         <Header />
         <section className="projects-main">
-          <div className="section-about">
-            <a className="projects" href="//www.linkedin.com/in/larissa-manzo/" alt="github">
+          <a className="section-about" href="//www.linkedin.com/in/larissa-manzo/" alt="github">
+            <div className="projects">
               Projeto Trybewallet
-            </a>
-          </div>
-          <div className="section-about">
-            <a className="projects" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-react-testing-library" alt="Projeto react testing">
+            </div>
+          </a>
+          <a className="section-about" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-react-testing-library" alt="Projeto react testing">
+            <div className="projects">
               Projeto react testing
-            </a>
-          </div>
-          <div className="section-about">
-            <a className="projects" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-trybetunes" alt="Projeto Trybetunes">
-              Projeto Trybetunes
-            </a>
-          </div>
-          <div className="section-about">
-            <a className="projects" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-movie-cards-library-stateful" alt="github">
+            </div>
+          </a>
+          <a className="section-about" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-trybetunes" alt="Projeto Trybetunes">
+            <div className="projects">
+                Projeto Trybetunes
+            </div>
+          </a>
+          <a className="section-about" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-movie-cards-library-stateful" alt="github">
+            <div className="projects">
               Projeto movie cards stateful
-            </a>
-          </div>
-          <div className="section-about">
-            <a className="projects" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-movie-cards-library" alt="github">
-            Projeto movie cards stateful
-            </a>
-          </div>
-          <div className="section-about">
-            <a className="projects" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-shopping-cart" alt="Projeto Shopping Cart">
-              Projeto Shopping Cart
-            </a>
-          </div>
-          <div className="section-about">
-            <a className="projects" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-zoo-functions" alt="Projeto Zoo Functions">
-              Projeto Zoo Functions
-            </a>
-          </div>
-          <div className="section-about">
-            <a className="projects" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-unit-tests" alt="Projeto JS unit tests">
-              Projeto JS unit tests
-            </a>
-          </div>
-          <div className="section-about">
-            <a className="projects" href="https://github.com/larymanzo/Projetos_trybe/tree/master/projetc-trybewarts" alt="projeto trybewarts">
-              Projeto trybewarts
-            </a>
-          </div>
-          <div className="section-about">
-            <a className="projects" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-todo-list" alt="projeto to do list">
-              Projeto to do list
-            </a>
-          </div>
-          <div className="section-about">
-            <a className="projects" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-pixels-art" alt="projeto pixels art">
-              Projeto pixels art
-            </a>
-          </div>
+            </div>
+          </a>
+          
+          <a className="section-about" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-movie-cards-library" alt="github">
+            <div className="projects">
+              Projeto movie cards stateful
+            </div>
+          </a>
+          <a className="section-about" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-shopping-cart" alt="Projeto Shopping Cart">
+            <div className="projects">
+                Projeto Shopping Cart
+            </div>
+          </a>
+          <a className="section-about" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-zoo-functions" alt="Projeto Zoo Functions">
+            <div className="projects">
+                Projeto Zoo Functions
+            </div>
+          </a>
+          <a className="section-about" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-unit-tests" alt="Projeto JS unit tests">
+            <div className="projects">
+                Projeto JS unit tests
+            </div>
+          </a>
+          <a className="section-about" href="https://github.com/larymanzo/Projetos_trybe/tree/master/projetc-trybewarts" alt="projeto trybewarts">
+            <div className="projects">
+                Projeto trybewarts
+            </div>
+          </a>
+          <a className="section-about" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-todo-list" alt="projeto to do list">
+            <div className="projects">
+                Projeto to do list
+            </div>
+          </a>
+          <a className="section-about" href="https://github.com/larymanzo/Projetos_trybe/tree/master/project-pixels-art" alt="projeto pixels art">
+            <div className="projects">
+                Projeto pixels art
+            </div>
+          </a>
         </section>
         <Footer />
       </>
